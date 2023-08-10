a8241b04574c81a963685fa830dd714debe11ee0
nometry <30988293+nometry@users.noreply.github.com>
2023-08-10 14:03:50 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -14,7 +14,7 @@ urls=(
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt
+  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/pro.plus.txt
 )
 
 # loop through the urls and download each file with curl
